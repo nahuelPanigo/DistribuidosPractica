7ab9dabbb6b48923a7bbca20dcc48df7f61582fe
Jimena Villanueva <villanuevajimena39@gmail.com>
2020-11-23 21:18:43 -0300

    pr4

--- pr4/AgenteMovil.java
+++ pr4/AgenteMovil.java
@@ -1,14 +1,18 @@
 import jade.core.*;
 import jade.wrapper.ContainerController;
+import com.sun.management.OperatingSystemMXBean; 
+import java.lang.management.ManagementFactory;
+import jade.lang.acl.ACLMessage;
+
 
 public class AgenteMovil extends Agent
 {
 static String[] vector = new String[6];
-
+	
 
 public void initializeArray(){
 	for (int i=2; i<6; i++ ){
-		vector[i] = "container-" + i;
+		vector[i] = "container-" + i ;
 	}
 	vector[0] = "Container-1";
 	vector[1] = "Main-Container";
@@ -16,16 +20,15 @@ public void initializeArray(){
 
 
 public String getDestino(String origen){
-	System.out.println(origen);
-	for(int i=0;i<6;i++){
-		if(origen.equals(vector[i])){
+	for (int i = 0; i<6; i++){
+		if(origen.equals(vector[i] + "@172.17.0.1")){
 			if(i == 5){
 				return vector[0];
 			}
 			return vector[i+1];
 		}
-	}
-	return "holaa";
+}
+		return "holaa";
 }
 
 public void createAllContainer(){
@@ -34,6 +37,20 @@ public void createAllContainer(){
 	}
 }
 
+public void sendMessage(String content){
+	ACLMessage message = new ACLMessage(ACLMessage.INFORM); 
+	message.setContent(content); 
+	// message.addReceiver(new AID(vector[0], AID.ISLOCALNAME)); 
+	send(message);
+}
+
+public void receiveAllMessages(){
+	ACLMessage msg = receive();
+	while (msg != null ) {
+		System.out.println(msg);
+		msg = receive();
+	}
+}
 
 public void setup()
 {
@@ -44,9 +61,8 @@ public void setup()
 	System.out.println("Y en location " + origen.getID() + "\n\n");
 	this.createAllContainer();
 try {
-	//System.out.println(this.getDestino(origen.getID()));
-	System.out.println("HASTA ACA LLEGO");
-	ContainerID destino = new ContainerID("Main-Container", null);
+	
+	ContainerID destino = new ContainerID(this.getDestino(origen.getID()), null);
 	System.out.println("Migrando el agente a " + destino.getID());
 	doMove(destino);
 	
@@ -68,13 +84,18 @@ protected ContainerController createContainer(String name) {
 
 protected void afterMove()
 {
+	OperatingSystemMXBean bean = (com.sun.management.OperatingSystemMXBean) ManagementFactory.getOperatingSystemMXBean();
+	this.initializeArray();
 	Location origen = here();
-	System.out.println("\n\nHola, agente migrado con nombre local " + getLocalName());
-	System.out.println("Y nombre completo... " + getName());
-	System.out.println("Y en location " + origen.getID() + "\n\n");
-	if( origen.getID().equals(vector[0])){
+	if( origen.getID().equals(vector[0] + "@172.17.0.1")){
+		this.receiveAllMessages();
 		System.out.println("termino. el resultado es");
 	}
-	
+	else {
+		ContainerID destino = new ContainerID(this.getDestino(origen.getID()), null);
+		String s = bean.getFreePhysicalMemorySize()+ origen.getID();
+		this.sendMessage(s);
+		doMove(destino);
+	}
 	}
 }
